f013e9bda78856f6ef5d93e3c2a84acc6e98f719
hanz1802 <134022235+hanz1802@users.noreply.github.com>
2023-12-01 22:07:01 -0500

    Add files via upload

--- js/animate.js
+++ js/animate.js
@@ -4,23 +4,23 @@ const cardsets = document.querySelectorAll('.card_container');
 
 const changePositions = (e, cardset) => {
     const clickedCard = e.currentTarget
-    if(clickedCard.classList.contains('active')) return;
-    const activeCard = cardset.querySelector('.cards.active')
+    if(clickedCard.classList.contains('activeCard')) return;
+    const activeCard = cardset.querySelector('.cards.activeCard')
     // const classesFrom = e.currentTarget.className
     // const classesTo = activeCard.className
 
     // clickedCard.className = classesTo
     clickedCard.classList.remove("right");
-    clickedCard.classList.add("active");
+    clickedCard.classList.add("activeCard");
     // activeCard.className = classesFrom
-    activeCard.classList.remove("active");
+    activeCard.classList.remove("activeCard");
     activeCard.classList.add("right");
 }
 
 cardsets.forEach((cardset) => {
     if (prefersReducedMotion.matches) return
     const cards = cardset.querySelectorAll('.cards')
-    const classes = ['active', 'right'];
+    const classes = ['activeCard', 'right'];
     cards.forEach((card, index) => card.classList.add(classes[index]))
 
 })
